f61d32018d52a8a26e1da6ce83ba3188b73cc2d0
Kritsakornn <77423011+Kritsakornn@users.noreply.github.com>
2022-03-31 20:43:35 +0700

    Exercise5_Kritsakorn_T

---
+++ Exercise5_1_Kritsakorn_T.py
@@ -0,0 +1,12 @@
+num_1 = int(input("Fill your first number: "))
+num_2 = int(input("Fill your second number: "))
+
+addition_total = num_1 + num_2
+subtraction_total = num_1 - num_2
+multiplication_total = num_1 * num_2
+division_total = num_1 / num_2
+
+print(num_1,"+",num_2,"=",addition_total)
+print(num_1,"-",num_2,"=",subtraction_total)
+print(num_1,"*",num_2,"=",multiplication_total)
+print(num_1,"/",num_2,"=",division_total)
---
+++ Exercise5_2_Kritsakorn_T.py
@@ -0,0 +1,5 @@
+s = int(input("Fill your distance: "))
+t = int(input("Fill your time: "))
+
+v = s/t
+print(v,"km/h")
