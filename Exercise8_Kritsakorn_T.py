0cca4246f2fe98ed608999868ec67f6026a27ed3
Kritsakornn <77423011+Kritsakornn@users.noreply.github.com>
2022-05-23 15:52:48 +0700

    Create Exercise8_Kritsakorn_T.py
    โปรแกรมร้านขายของโดยดัดแปลงจากโปรแกรมใน Lecture 40

---
+++ Exercise8_Kritsakorn_T.py
@@ -0,0 +1,24 @@
+username = input("Username : ")
+password = input("Password : ")
+
+if username == "Kritsakorn" and password == "123456789":
+    print("Login success")
+    print("Welcome to the dev store")
+    print("-----Choose your goods-----")
+    print("1. Chicken 300THB")
+    print("2. Meat 200THB")
+    print("3. Tomato 50THB")
+    userSelected = int(input("Type the number : "))
+    total_of_goods = int(input("How many do you want to buy? : "))
+    chicken_price = 300
+    meat_price = 200
+    tomato_price = 50
+    if userSelected == 1:
+        price = chicken_price * total_of_goods
+        print("you need to pay " + str(price) + "THB")
+    elif userSelected == 2:
+        price = meat_price * total_of_goods
+        print("you need to pay " + str(price) + "THB")
+    elif userSelected == 3:
+        price = tomato_price * total_of_goods
+        print("you need to pay " + str(price) + "THB")
