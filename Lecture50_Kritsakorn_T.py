948dd728674eb4fd1ab8643192abcc3d911616af
Kritsakornn <77423011+Kritsakornn@users.noreply.github.com>
2022-06-05 13:12:45 +0700

    Create Lecture50_Kritsakorn_T.py

---
+++ Lecture50_Kritsakorn_T.py
@@ -0,0 +1,13 @@
+def addnumber(x,y):
+    print(x+y)
+def subtractNumber(x,y):
+    print(x-y)
+def mutipleNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addnumber(10,2)
+subtractNumber(10,2)
+mutipleNumber(10,2)
+divideNumber(10,2)
