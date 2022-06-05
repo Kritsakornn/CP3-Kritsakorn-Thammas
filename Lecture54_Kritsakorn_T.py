1ab06587ca0456d1c5ad2c7f8f95a74c1262e586
Kritsakornn <77423011+Kritsakornn@users.noreply.github.com>
2022-06-05 18:06:58 +0700

    Create Lecture54_Kritsakorn_T.py

---
+++ Lecture54_Kritsakorn_T.py
@@ -0,0 +1,33 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+def vatCalculate(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * 7 / 100)
+    return result
+def priceCalculate():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculate(price1 + price2)
+
+login()
+showMenu()
+UserSelected = menuSelect()
+if UserSelected == 1:
+    totalPrice = int(input("Price (THB): "))
+    print(vatCalculate(totalPrice))
+elif UserSelected == 2:
+    print(priceCalculate())
+
+
